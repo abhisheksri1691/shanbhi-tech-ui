b597cc2347b6276ef05a082b26f6e7397cbdf4df
abhishek srivastava <abhisheksri.dev@gmail.com>
2023-01-27 23:32:36 +0530

    addded main contetnt

---
+++ pages/HeroHome.jsx
@@ -0,0 +1,82 @@
+
+function HeroHome() {
+
+
+  return (
+    <section className="relative">
+
+      {/* Illustration behind hero content
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
+        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div> */}
+
+      <div className="max-w-full mx-auto px-4 sm:px-6 bg-gradient-to-r from-slate-300 to-teal-300">
+
+        {/* Hero content */}
+        <div className="pt-32 pb-12 md:pt-20 md:pb-20">
+
+          {/* Section header */}
+          <div className="text-center pb-12 md:pb-16">
+            <h1 className="text-6xl md:text-8xl font-extrabold leading-tighter tracking-tighter mb-4 text-sky-900" >Make your business<p><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-slate-800">wonderful</span></p></h1>
+            <div className="max-w-3xl mx-auto">
+              <p className="text-base md:text-xl text-gray-900 mb-8" >Grow your business more with new technology softwares, we provide not only all IT solutions, provide trust and beautiful bonding with out client, set it up once, and get beautiful results forever.</p>
+            </div>
+          </div>
+
+          {/* Hero image */}
+
+            {/* <div className="relative flex justify-center">
+                <svg className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto" width="768" height="432" viewBox="0 0 768 432" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                  <defs>
+                    <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="hero-ill-a">
+                      <stop stopColor="#FFF" offset="0%" />
+                      <stop stopColor="#EAEAEA" offset="77.402%" />
+                      <stop stopColor="#DFDFDF" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="50%" y1="0%" x2="50%" y2="99.24%" id="hero-ill-b">
+                      <stop stopColor="#FFF" offset="0%" />
+                      <stop stopColor="#EAEAEA" offset="48.57%" />
+                      <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
+                    </linearGradient>
+                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="hero-ill-e">
+                      <stop stopColor="#4FD1C5" offset="0%" />
+                      <stop stopColor="#81E6D9" offset="25.871%" />
+                      <stop stopColor="#338CF5" offset="100%" />
+                    </radialGradient>
+                    <circle id="hero-ill-d" cx="384" cy="216" r="64" />
+                  </defs>
+                  <g fill="none" fillRule="evenodd">
+                    <circle fillOpacity=".04" fill="url(#hero-ill-a)" cx="384" cy="216" r="128" />
+                    <circle fillOpacity=".16" fill="url(#hero-ill-b)" cx="384" cy="216" r="96" />
+                    <g fillRule="nonzero">
+                      <use fill="#000" xlinkHref="#hero-ill-d" />
+                      <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
+                    </g>
+                  </g>
+                </svg>
+            
+            </div> */}
+
+
+          
+
+        </div>
+
+      </div>
+    </section>
+  );
+}
+
+export default HeroHome;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import Form from "./Form";
 import Header from "./header";
 import MyFooter from "./MyFooter";
 import Registration from "./Registration";
+import HeroHome from "./HeroHome";
 
 export default function Home() {
   return (
     <>
-    <Registration/> 
+    <HeroHome/>
+    {/* <Registration/>  */}
     </>
   )
 }
